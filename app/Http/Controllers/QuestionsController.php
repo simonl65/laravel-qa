113f63ee3a780a358369dabf62c36730cdb69fcb
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-25 14:27:13 +0100

    Completed the destroy() function

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -90,6 +90,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect(route('questions.index'))->with('success', "That question has now been deeted.");
     }
 }
