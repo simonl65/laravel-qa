7529cd04004c1064f4bf0661f889a2fa8b732408
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-25 10:28:30 +0100

    Added 'Ask Question' button to card header

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -4,7 +4,15 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card mb-1">
-                <div class="card-header">All Questions <span class="order-1"></span></div>
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
                     @foreach ($questions as $question)
